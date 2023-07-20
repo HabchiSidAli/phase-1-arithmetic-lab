968068f25a3518a0cb730f355b1176296b99890a
HabchiSidAli <sidalihabchi96@gmail.com>
2023-07-20 20:54:16 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+//multiply
+let num1, num2, num3, num4, num5;
 
-// Write your code here
+num1 = 31;
+num2 = 2;
+
+let multiply = num1 * num2;
+
+//random
+let random = Math.round(Math.random() * 10) + 1;
+
+//mod
+num3 = 24;
+num4 = 5;
+
+let mod = num3 % num4;
+
+//max
+let max = Math.max(8, 5, 20);
